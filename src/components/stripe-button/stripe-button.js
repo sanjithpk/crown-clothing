975dbe23f09ee34b17248d57f2bb835c89722d1d
sanjithpk <sanjithpk06@gmail.com>
2019-07-21 17:47:43 +0530

    Integrated with Stripe API and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.js
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_oLwD0jZb7iGPu2pC31z0IDn900HSQKuxvY";
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
