cebf9332a28feeda5b09bef62cc5623335cb6692
sanjithpk <sanjithpk06@gmail.com>
2020-01-13 22:02:18 +0530

    added header componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
